645744907fc9a8a07cbfa89ef926716edcf63832
Gerard Rodes <grv94sk@gmail.com>
2017-05-08 21:42:11 +0200

    added tensorflow, switch to python3

--- format_dictionaries.py
+++ format_dictionaries.py
@@ -7,7 +7,7 @@ import codecs
 FOLDER = './dictionaries'
 NUMBERS = [str(number) for number in range(10)]
 CHARACTERS = [' ', '.', '-', '\'', '#', '!', '"', '·', '$', '%', '&', '/', '(', ')', '=', '?', '¿', '|', '@', '#', '¬']
-FORBIDDEN = [char.decode('utf-8') for char in NUMBERS + CHARACTERS]
+FORBIDDEN = [char for char in NUMBERS + CHARACTERS]
 
 
 
@@ -15,7 +15,7 @@ dictionaries = [dictionary for dictionary in os.listdir(FOLDER)
                 if os.path.isfile( os.path.join(FOLDER, dictionary) ) and dictionary.endswith('.txt') ]
 
 for dictionary in dictionaries:
-  print 'Formatting file {file}'.format(file = dictionary)
+  print( 'Formatting file {file}'.format(file = dictionary) )
   
   dictionary_filename = os.path.join(FOLDER, dictionary)
   temp_filename       = os.path.join(FOLDER, dictionary + '.temp')
@@ -24,7 +24,7 @@ for dictionary in dictionaries:
   with codecs.open(dictionary_filename, 'r', 'utf-8') as file:
     with codecs.open(temp_filename, 'w', 'utf-8') as temp_file:
       for index, line in enumerate(file):
-        print '{current}/{total}'.format(current = index + 1, total = total_lines)
+        print( '{current}/{total}'.format(current = index + 1, total = total_lines) )
         
         if len( line.rstrip() ) > 3:
           forbidden_chars = [char for char in line if char in FORBIDDEN]
